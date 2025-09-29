5eb452f604472ae28f10e4173a005c114d4c8b78
Hadeel <ha.ada1@hotmail.com>
2025-09-29 11:32:52 +0300

    jo

---
+++ 12/place details.swift
@@ -0,0 +1,18 @@
+//
+//  place details.swift
+//  12
+//
+//  Created by Hadeel Alansari on 29/09/2025.
+//
+
+import SwiftUI
+
+struct place_details: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    place_details()
+}
