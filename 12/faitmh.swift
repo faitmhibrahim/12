55e2277d4e0ca9ac5789a9c7d050596fc0a85726
Reemi <121254688+rareemi@users.noreply.github.com>
2025-09-29 11:40:54 +0300

    f

---
+++ 12/faitmh.swift
@@ -0,0 +1,18 @@
+//
+//  faitmh.swift
+//  12
+//
+//  Created by Faitmh ibrahim on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+struct faitmh: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    faitmh()
+}
